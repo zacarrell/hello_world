6d1096b8be0ef6c19d08341b172ef59f80293bb6
Garrett Dancik <garrett.dancik@gmail.com>
2017-10-22 18:32:57 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# hello_world
+
+This is a test repository to be used by ECSU students
+
+
---
+++ hello.java
@@ -0,0 +1,13 @@
+// a simple program to practice common Github commands 
+
+public class hello {
+
+    public static void main(String [] args) {
+
+        System.out.println("Hello Class!");
+
+        System.out.println("Bye");
+
+    }
+}
+
