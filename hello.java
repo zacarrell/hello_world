b5145ff5341aa89e800bfdbc62b9efcd77bd8953
Zachary A. Carrell <zacarrell@yahoo.com>
2017-10-23 17:08:17 -0400

    added prompt for username

--- hello.java
+++ hello.java
@@ -1,16 +1,18 @@
 // a simple program to practice common Github commands 
+import java.util.Scanner;
 
 public class hello {
 
     public static void main(String [] args) {
 
-
-	// added  line	
-
         System.out.println("Hello Class!");
 
-
-
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Hi there!  What's your name?  ");
+		String name = reader.next();
+		
+		System.out.println("Hey, " + name + "!  My name is Zac.");
+		
     }
 }
 
